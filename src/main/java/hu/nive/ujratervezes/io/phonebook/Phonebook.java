f5ef608e350af0f3c99d4c519b2c6d5302d4cdc5
DurayBence <duray.bence@users.noreply.github.com>
2021-04-29 10:54:56 +0200

    Complete task

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,34 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+
+        File file = new File(output);
+
+        try {
+            file.delete();
+            file.createNewFile();
+            FileWriter fileWriter = new FileWriter(file, true);
+
+            for (Map.Entry<String,String> entry : contacts.entrySet()) {
+                fileWriter.append(entry.getKey());
+                fileWriter.append(": ");
+                fileWriter.append(entry.getValue());
+                fileWriter.append("\n");
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("There was an error!");
+        }
+    }
+
+}
